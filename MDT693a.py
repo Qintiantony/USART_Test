a7e69e3e9f009c44407be3ff57ef2d190ed9feed
Qintiantony <36404166+Qintiantony@users.noreply.github.com>
2018-05-21 14:33:06 +0800

    work

--- MDT693a.py
+++ MDT693a.py
@@ -2,21 +2,18 @@ import serial.tools.list_ports
 import serial
 import sys
 import time
+import struct
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-plist= list(serial.tools.list_ports.comports())
-if len(plist)<=0:
-    print("No serial port founded!")
-else:
-    serial_port=list(plist[0])[0]
-    MDT693a=serial.Serial(serial_port,baudrate=115200,bytesize=8,stopbits=1,parity='N',timeout=2)
-    MDT693a.setDTR(True)
-    MDT693a.setRTS(True)
-    MDT693a.setRTS(False)
-    MDT693a.setDTR(False)
-    print("Serial port found: "+MDT693a.name)
-MDT693a.write("ZV?".encode())
-print(bytes.decode(MDT693a.read_all()))
+MDT693a=serial.Serial("COM5",baudrate=115200,bytesize=8,stopbits=1,parity='N',timeout=5)
+MDT693a.setDTR(True)
+MDT693a.setRTS(True)
+MDT693a.setRTS(False)
+MDT693a.setDTR(False)
+MDT693a.write(bytes().fromhex('050000002101'))
+result=MDT693a.readall()
+serial_number=struct.unpack('<L', bytes(result[6:10]))[0]
+print(serial_number)
 MDT693a.close()
