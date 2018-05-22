2c3eafb37bf8e8b570cfabf3ebdfcaa804b1baa4
Qintiantony <36404166+Qintiantony@users.noreply.github.com>
2018-05-22 11:12:55 +0800

    1.2

--- MDT693a.py
+++ MDT693a.py
@@ -7,13 +7,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-MDT693a=serial.Serial("COM5",baudrate=115200,bytesize=8,stopbits=1,parity='N',timeout=5)
-MDT693a.setDTR(True)
-MDT693a.setRTS(True)
-MDT693a.setRTS(False)
-MDT693a.setDTR(False)
+MDT693a=serial.Serial("COM6",baudrate=115200,bytesize=8,stopbits=1,parity='N',timeout=1)
+#MDT693a.setDTR(True)
+#MDT693a.setRTS(True)
+#MDT693a.setRTS(False)
+#MDT693a.setDTR(False)
+result=0
 MDT693a.write(bytes().fromhex('050000002101'))
-result=MDT693a.readall()
+result=MDT693a.readline()
 serial_number=struct.unpack('<L', bytes(result[6:10]))[0]
 print(serial_number)
+#MDT693a.write(bytes().fromhex('400601032101'))
+MDT693a.write(bytes().fromhex('410601002101'))
+result=MDT693a.readline()
+print([hex(x) for x in result])
+#time.sleep(1)
+MDT693a.write(bytes().fromhex('43060400D00101001717'))
+#time.sleep(1)
+MDT693a.write(bytes().fromhex('440601002101'))
+result=MDT693a.readline()
+print([hex(x) for x in result])
 MDT693a.close()
